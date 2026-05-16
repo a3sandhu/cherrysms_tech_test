11997fb2144b76125abd730b81a5eacb2f9d1b3d
Apnambir Sandhu <a3sandhu@ucsd.edu>
2026-05-15 22:10:24 -0700

    rush-1-1 logic implemented without my_putchar() and update gitignore

--- rush-1-1/rush-1-1.c
+++ rush-1-1/rush-1-1.c
@@ -1,9 +1,51 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int my_putchar (int ch) {
-
+    return 0;
 }
 
-void rush (int x, int y) {
-    
+void rowPrinter(int cols, int isTopOrBottom)
+{
+    for(int curCol = 0; curCol<cols; ++curCol)
+    {
+        int onEdge = (curCol == 0 || curCol == cols-1);
+
+        if(onEdge && isTopOrBottom)
+        {
+            my_putchar('o');
+        }
+
+        else if(isTopOrBottom)
+        {
+            my_putchar('-');
+        }
+
+        else if(onEdge)
+        {
+            my_putchar('|');
+        }
+        else
+        {
+            my_putchar(' ');
+        }
+    }
+}
+
+void rush (int rows, int cols) 
+{
+    if (rows <= 0 || cols <= 0) 
+    {
+        fprintf(stderr, "Invalid size\n");
+        return;
+    }
+    for(int curRow=0; curRow<rows; ++curRow)
+    {
+        int isTopOrBottom = (curRow == 0 || curRow == rows-1);
+        rowPrinter(cols,isTopOrBottom);
+    }
+}
+int main() {
+    rush(1,1);
+    return 0;
 }
